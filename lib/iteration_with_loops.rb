b08d86f76feeab069e620e3d6d7c6ed98e333fe5
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-12 13:05:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,9 +7,8 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count do
-      if src[row_index][element_index] <= src[row_index].sort.first 
-        new_src << src[row_index][element_index]
-      end 
+      new src << src[row_index].sort.first 
+      
       element_index += 1 
     end 
     row_index += 1 
